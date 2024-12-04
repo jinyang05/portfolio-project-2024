2a2038e56b295a08d343e756a6c2bfe15f35c851
Jin Yang <jinyang01215@gmail.com>
2024-12-03 20:35:14 -0500

    added test cases for all methods

---
+++ src/components/MusicPlaylist/MusicPlaylistKernel.java
@@ -0,0 +1,68 @@
+import components.map.Map;
+import components.standard.Standard;
+
+/**
+ * Music Playlist component with primary methods.
+ *
+ */
+public interface MusicPlaylistKernel extends Standard<MusicPlaylist> {
+    /**
+     * Adds {@code (song, artist)} to {@code this}.
+     *
+     * @param song
+     *            the {@code song} to be added
+     * @param artist
+     *            the {@code artist} to be added
+     * @updates this
+     * @ensures |this| = |#this| + 1
+     */
+    void add(String song, String artist);
+
+    /**
+     * Removes {@code song} from {@code this}.
+     *
+     * @param song
+     *            the {@code song} to be removed
+     * @updates this
+     * @ensures |this| = |#this| - 1
+     * @requires |this| > 0 and song is in DOMAIN (this)
+     * @return the song and artist removed
+     */
+    Map.Pair<String, String> remove(String song);
+
+    /**
+     * Reports whether {@code this} is empty.
+     *
+     * @ensures isEmpty = (|this| = 0)
+     * @return true iff {@code this} is empty
+     */
+    boolean isEmpty();
+
+    /**
+     * Removes an arbitrary song from {@code this}.
+     *
+     * @updates this
+     * @ensures |this| = |#this| - 1
+     * @requires |this| > 0
+     * @return the artist and song removed
+     */
+    Map.Pair<String, String> removeAny();
+
+    /**
+     * Reports whether {@code song} is in {@code this}.
+     *
+     * @param song
+     *            the {@code song} that is being checked
+     * @return trur iff {@code song} is in {@code this}
+     */
+    boolean contains(String song);
+
+    /**
+     * Reports size of {@code this}.
+     *
+     * @return the size of {@code this}.
+     * @ensures |this| = @return
+     */
+    int size();
+
+}
---
+++ src/components/MusicPlaylist/MusicPlaylistSecondary.java
@@ -0,0 +1,75 @@
+import components.map.Map;
+
+/**
+ * Layered implementations of secondary methods for {@code MusicPlaylist}.
+ */
+public abstract class MusicPlaylistSecondary implements MusicPlaylist {
+
+    @Override
+    public final void shuffle() {
+        Map.Pair<String, String> removed = this.removeAny();
+        this.add(removed.value(), removed.key());
+    };
+
+    @Override
+    public final String artist(String song) {
+        Map.Pair<String, String> removed = this.remove(song);
+        String artist = removed.key();
+        this.add(removed.value(), removed.key());
+        return artist;
+    };
+
+    @Override
+    public final String upNext() {
+        Map.Pair<String, String> removed = this.removeAny();
+        String song = removed.value();
+        this.add(removed.value(), removed.key());
+        return song;
+    };
+
+    @Override
+    public final String toString() {
+        throw new UnsupportedOperationException(
+                "toString() can not be implemented");
+    };
+
+    @Override
+    public final boolean equals(Object musicPlaylist) {
+        if (musicPlaylist == null) {
+            return false;
+        }
+
+        if (musicPlaylist == this) {
+            return true;
+        }
+
+        if (!(musicPlaylist instanceof MusicPlaylist)) {
+            return false;
+        }
+
+        MusicPlaylist underTest = (MusicPlaylist) musicPlaylist;
+        if (this.size() != underTest.size()) {
+            return false;
+        }
+        //MusicPlaylist temp = this.newInstance();
+        if (this.size() == underTest.size() && !this.isEmpty()) {
+            // remove elements until empty
+            while (!underTest.isEmpty()) {
+                Map.Pair<String, String> removed = underTest.removeAny();
+                // if the elment is also in this, remove it
+                if (!this.contains(removed.value())) {
+                    return false;
+                }
+            }
+        } else if (this.isEmpty() && underTest.isEmpty()) {
+            return true;
+        }
+        return false;
+    };
+
+    @Override
+    public final int hashCode() {
+        return this.size();
+    }
+
+}
